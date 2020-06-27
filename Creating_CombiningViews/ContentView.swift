74e1a340c602e9e45f33d5f2253c73600b54e01d
Miguel Rojas <migueldrojascortes@icloud.com>
2020-06-27 10:10:37 -0500

    Initial Commit

---
+++ Creating_CombiningViews/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Creating_CombiningViews
+//
+//  Created by Miguel on 27/06/20.
+//  Copyright © 2020 Miguel. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
